cbc00731ef5f0e80aa890174ef3f40ab576c2690
huangleah <33859881+huangleah@users.noreply.github.com>
2018-01-25 18:07:22 +0800

    Update Ex1.3-1.4 c to f conversion.c

--- Ex1.3-1.4 c to f conversion.c
+++ Ex1.3-1.4 c to f conversion.c
@@ -7,7 +7,7 @@ main()
 	printf("c to f conversion\n");
 	for (c = lower; c <= upper; c = c + step)
 	{
-		f = 32+(9.0/7.0)*c;
+		f = 32+(9.0/7.0)*c; /*float 9 and 7 is necessary for achieving float type results */
 		
 		printf("%3.0f\t%3.3f\n", c, f);
 	}
