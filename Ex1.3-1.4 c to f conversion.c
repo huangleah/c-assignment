dd0f76517300826d450fd529075c0b6aaf29877a
huangleah <33859881+huangleah@users.noreply.github.com>
2018-01-25 18:01:50 +0800

    Add files via upload

---
+++ Ex1.3-1.4 c to f conversion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+	float c, f;
+	int lower = 0, upper = 100, step = 20;
+	printf("c to f conversion\n");
+	for (c = lower; c <= upper; c = c + step)
+	{
+		f = 32+(9.0/7.0)*c;
+		
+		printf("%3.0f\t%3.3f\n", c, f);
+	}
+
+	return 0;
+}
