9f733c2221fd6113a3fa169fe242665bcf187b92
huangleah <33859881+huangleah@users.noreply.github.com>
2018-01-26 19:02:54 +0800

    Add files via upload

---
+++ Ex.2.1 find out ranges for data types.c
@@ -0,0 +1,21 @@
+/*src:page 213 B.11 */
+
+#include <stdio.h>
+#include <limits.h>
+main()
+{
+	
+	printf("Max value of signed char is %d\n", SCHAR_MAX);
+	printf("Min value of signed char is %d\n", SCHAR_MIN);
+	printf("Max value of signed int is %d\n", INT_MAX);
+	printf("Min value of signed int is %d\n", INT_MIN);
+	printf("Max value of singed long is %d\n", LONG_MAX);
+	printf("Min value of signed long is %d\n", LONG_MIN);
+	printf("Max value of unsigned long is %d\n", ULONG_MAX);
+	printf("Max value of unsigned int is %d\n", UINT_MAX);
+    printf("Max value of unsigned char is %d\n", UCHAR_MAX);
+
+
+
+
+}
