1331e2de357380ac71f3163a339248d73ba3c350
huangleah <33859881+huangleah@users.noreply.github.com>
2018-02-10 23:24:52 +0800

    Add files via upload

---
+++ Ex5.3 2nd expression.c
@@ -0,0 +1,26 @@
+/*Ex5.3, pointer version of strcat */
+
+#include <stdio.h>
+
+void strcat(char *s, char *t)
+{
+	
+	while (*s++ != '\0')
+		;
+
+	s--;
+	while ( (*s++ = *t++)!= '\0')
+		;
+}
+void main()
+{
+	char s1[1000] = "this is not an apple";
+	char t1[500] = "this is an apple";
+
+	strcat(s1, t1);
+
+	printf("%s\n", s1);
+	printf("%s\n", t1);
+
+}
+
